8e649d5e197b21e16046727ebf5d71220422e30c
maricolman <marielacolman95@gmail.com>
2016-09-06 19:04:01 -0400

    tarea05

---
+++ js/app.js
@@ -0,0 +1,25 @@
+//#04 Declaran funcin anónima autoejecutable
+console.log('Hola Mundo');
+
+//#05 Variable tipo array
+//#08 Añadir elementos string
+//#09 Imprimir en consola
+var array=[];
+	array.push('Tarea 5');
+	array.push('Exelente');
+
+console.log(array);
+
+//#06 Variable string
+var nombre = 'Mariela';
+
+//#07 Variable numerica
+var edad = 20;
+
+//#10 Crear un variable object
+//#11 Añadir atributos al object
+var objeto={};
+	objeto.descripcion = 'Calificación'
+	objeto.cantidad = 5
+
+console.log(objeto);
